6bb54644cf10b8395f5dbec625eba9f84c675328
Alexandre Malkov <sm1030@gmail.com>
2018-07-22 10:24:00 +0100

    Create initial folder structure

---
+++ MarvelComics/MarvelComics/UILayer/ViewControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarvelComics
+//
+//  Created by Alexandre Malkov on 22/07/2018.
+//  Copyright © 2018 alexmalkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
